1b5cd3358fd4392ea83b962c90391505500c2f6b
Kavan Bickerstaff <kavanbickerstaff@googlemail.com>
2016-01-28 18:22:02 +0000

    Basic window setup (needs recompiled SDL2 source)

--- GameEngine/GameEngine/main.cpp
+++ GameEngine/GameEngine/main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+#include <SDL.h>
+#include <stdio.h>
 
-int main()
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
 {
-	std::cout << "hello beautiful world";
+	SDL_Window* window = nullptr;
+
+	SDL_Surface* screen_surface = nullptr;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not be initialized! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		window = SDL_CreateWindow("Game Engine",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH, SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		if (!window)
+		{
+			printf("SDL window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			screen_surface = SDL_GetWindowSurface(window);
+
+			SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+
+			SDL_UpdateWindowSurface(window);
+
+			SDL_Delay(2000);
+		}
+	}
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+
+	return 0;
 }
