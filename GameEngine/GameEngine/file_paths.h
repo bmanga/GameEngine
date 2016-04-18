ed1fd78e35dea4ee1c8b4907430809869c4ba243
Bruno Manganelli <bruno.manga95@gmail.com>
2016-04-18 02:50:15 +0100

    Confused

--- GameEngine/GameEngine/file_paths.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <filesystem>
-
-namespace Lemur {
-namespace fs = std::experimental::filesystem;
-static const fs::path MESH_PATH("..\\assets\\mesh");
-static const fs::path TEXTURE_PATH("..\\assets\\textures");
-static const fs::path SHADER_PATH("..\\assets\\shaders");
-};
